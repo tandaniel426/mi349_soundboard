e376adf11a2819761f64298a12bfc0f3f7b95eb1
tandaniel426 <tandanie@msu.edu>
2020-11-12 15:34:34 -0500

    Added initial functionality

---
+++ soundboard.js
@@ -0,0 +1,15 @@
+function playHorn () {
+    document.getElementById("horn-sound").play();
+}
+
+function playAlarm () {
+    document.getElementById("alarm-sound").play();
+}
+
+function playBell() {
+    document.getElementById("bell-sound").play();
+}
+
+function playCheering () {
+    document.getElementById("cheering-sound").play();
+}
