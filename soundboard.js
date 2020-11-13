ec092861cd42ef614c1f854dd7c77c01128d664d
tandaniel426 <tandanie@msu.edu>
2020-11-13 16:48:29 -0500

    changes to JS and CSS

--- soundboard.js
+++ soundboard.js
@@ -13,3 +13,40 @@ function playBell() {
 function playCheering () {
     document.getElementById("cheering-sound").play();
 }
+
+let hornButton = document.getElementById("horn");
+let alarmButton = document.getElementById("alarm");
+let bellButton = document.getElementById("bell");
+let cheeringButton = document.getElementById("cheering");
+
+hornButton.onmouseenter = function () {
+    playHorn();
+}
+
+hornButton.onclick = function () {
+    playHorn();
+}
+
+alarmButton.onmouseenter = function () {
+    playAlarm();
+}
+
+alarmButton.onclick = function () {
+    playAlarm();
+}
+
+bellButton.onmouseenter = function () {
+    playBell();
+}
+
+bellButton.onclick = function () {
+    playBell();
+}
+
+cheeringButton.onmouseenter = function () {
+    playCheering();
+}
+
+cheeringButton.onclick = function () {
+    playCheering();
+}
